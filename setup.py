0d2ccb243981a089c39dec2ca0b853572cd07b29
Khirawadhi <khirawdhi@gmail.com>
2018-04-11 06:01:32 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='SSL_Scanner',
+      version='0.1',
+      description='Save it.',
+      url='http://mad.com',
+      author='Flying Circus',
+      author_email='hooman@example.com',
+      license='BPUT',
+      packages=['SSL_Scanner'],
+      zip_safe=False)
